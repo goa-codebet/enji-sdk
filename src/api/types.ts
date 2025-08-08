1495454da08c3c0eefbbbbe9f6e7f6b8ad212532
Jonathan Frey <jonathan.k.frey@gmail.com>
2025-08-08 10:56:50 +0200

    export types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,3 +1,20 @@
+export * from "@/api/clients/BonusClient/types";
+export * from "@/api/clients/CasinoClient/types";
+export * from "@/api/clients/CasinoTournamentClient/types";
+export * from "@/api/clients/ContentClient/types";
+export * from "@/api/clients/ContentClient/types";
+export * from "@/api/clients/EventFeedClient/types";
+export * from "@/api/clients/ExchangeRatesClient/types";
+export * from "@/api/clients/InternalMessagingClient/types";
+export * from "@/api/clients/KycClient/types";
+export * from "@/api/clients/PlayerClient/types";
+export * from "@/api/clients/PlayerPropertiesClient/types";
+export * from "@/api/clients/ResponsibleGamingClient/types";
+export * from "@/api/clients/ResponsibleGamingLimitClient/types";
+export * from "@/api/clients/SportsbookClient/types";
+export * from "@/api/clients/TransactionClient/types";
+export * from "@/api/clients/WalletClient/types";
+
 export type Currency =
   | "Default"
   | "EUR"
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["src/index.ts", "src/api/index.ts"],
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   minify: true,
   clean: true,
-  external: ["react"],
   ...options,
 }));
